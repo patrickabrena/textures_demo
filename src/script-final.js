b51340650958a1c9edffb68123b8b7ed3b1393c1
Patrick Abrena <patrickabrena@hotmail.com>
2025-03-20 23:40:40 -0500

    fix orbit controls import

--- src/script-final.js
+++ src/script-final.js
@@ -1,6 +1,6 @@
 // Import required modules
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 import { Pane } from "tweakpane";
